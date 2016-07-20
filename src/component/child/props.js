e92377ca67bf59f8674766b80c52d42fa1d6bc9e
Daniel Brain <dbrain@paypal.com>
2016-07-20 15:07:45 -0700

    Add missing file

---
+++ src/component/child/props.js
@@ -0,0 +1,38 @@
+
+import { SyncPromise as Promise } from 'sync-browser-mocks/src/promise';
+import { getUrlParam } from '../../lib';
+import { PROP_DEFER_TO_URL } from '../../constants';
+
+export function normalizeChildProps(component, props) {
+
+    let result = {};
+
+    for (let key of Object.keys(props)) {
+
+        let prop = component.props[key];
+        let value = props[key];
+
+        if (value === PROP_DEFER_TO_URL) {
+
+            let queryParam = key;
+
+            if (typeof prop.queryParam === 'string') {
+                queryParam = prop.queryParam;
+            }
+
+            value = getUrlParam(queryParam);
+            if (prop.getter) {
+                let val = Promise.resolve(value);
+                value = function()  {
+                    return val;
+                };
+            }
+        }
+
+        result[key] = value;
+    }
+
+    return result;
+}
+
+
