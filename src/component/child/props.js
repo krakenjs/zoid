e7ac0b90ef46c6d4164a872d8cfc33bb4ea57d16
Daniel Brain <dbrain@paypal.com>
2016-08-25 17:41:09 -0700

    Make sure PROP_DEFER_TO_URL also works for getters

--- src/component/child/props.js
+++ src/component/child/props.js
@@ -1,5 +1,4 @@
 
-import { SyncPromise as Promise } from 'sync-browser-mocks/src/promise';
 import { getQueryParam } from '../../lib';
 import { PROP_DEFER_TO_URL } from '../../constants';
 
@@ -12,21 +11,22 @@ export function normalizeChildProps(component, props) {
         let prop = component.props[key];
         let value = props[key];
 
+        let queryParam = (typeof prop.queryParam === 'string') ? prop.queryParam : key;
+
         if (value === PROP_DEFER_TO_URL) {
-
-            let queryParam = key;
-
-            if (typeof prop.queryParam === 'string') {
-                queryParam = prop.queryParam;
-            }
-
             value = getQueryParam(queryParam);
-            if (prop.getter) {
-                let val = Promise.resolve(value);
-                value = function()  {
-                    return val;
-                };
-            }
+        }
+
+        if (prop.getter && value) {
+            let val = value;
+            value = function() {
+                return val().then(res => {
+                    if (res === PROP_DEFER_TO_URL) {
+                        return getQueryParam(queryParam);
+                    }
+                    return result;
+                });
+            };
         }
 
         result[key] = value;
