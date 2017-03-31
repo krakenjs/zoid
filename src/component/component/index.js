4cc8b5fa9776da06485831f2de4000f92db24058
Daniel Brain <dbrain@paypal.com>
2017-03-30 17:10:21 -0700

    Add containerTemplate default to the correct prop

--- src/component/component/index.js
+++ src/component/component/index.js
@@ -101,8 +101,8 @@ export class Component extends BaseComponent {
 
         // Templates and styles for the parent page and the initial rendering of the component
 
-        this.addProp(options, 'containerTemplate');
-        this.addProp(options, 'componentTemplate', ({ CLASS }) => `<div class="${ CLASS.ELEMENT }"></div>`);
+        this.addProp(options, 'containerTemplate', ({ CLASS }) => `<div class="${ CLASS.ELEMENT }"></div>`);
+        this.addProp(options, 'componentTemplate');
 
         this.addProp(options, 'sacrificialComponentTemplate', false);
 
