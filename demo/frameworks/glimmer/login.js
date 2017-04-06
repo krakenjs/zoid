d8771ac6aef9e678ce9668aeb886153113387c8d
Daniel Brain <dbrain@paypal.com>
2017-04-06 01:05:43 -0700

    Add placeholder for glimmer demo

---
+++ demo/frameworks/glimmer/login.js
@@ -0,0 +1,11 @@
+
+window.MyLoginXComponent = xcomponent.create({
+
+    // The html tag used to render my component
+
+    tag: 'my-login-component',
+
+    // The url that will be loaded in the iframe or popup, when someone includes my component on their page
+
+    url: xcomponent.getCurrentScriptDir() + '/login.htm'
+});
