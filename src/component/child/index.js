84617a37a968b6eae7ad0786f79007a4a6eb5028
Daniel Brain <dbrain@paypal.com>
2017-04-25 09:16:11 -0700

    Instantly run cleanup tasks if cleanup has already been triggered

--- src/component/child/index.js
+++ src/component/child/index.js
@@ -137,7 +137,7 @@ export class ChildComponent extends BaseComponent {
     setProps(props = {}, origin, required = true) {
         window.xprops = this.props = this.props || {};
         extend(this.props, normalizeChildProps(this.component, props, origin, required));
-        this.props.onError = (err) => this.error(err);
+        // this.props.onError = (err) => this.error(err);
         for (let handler of this.onPropHandlers) {
             handler.call(this, this.props);
         }
