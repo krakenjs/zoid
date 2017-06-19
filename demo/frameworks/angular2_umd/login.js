511bf1037e85d2b6e6cd00b44460b6569a72ff9d
Ahmed Elharouny <ahmedelharouny@yahoo.com>
2017-06-19 17:08:30 +1000

    add ng umd demo

---
+++ demo/frameworks/angular2_umd/login.js
@@ -0,0 +1,24 @@
+
+window.MyLoginXComponent = xcomponent.create({
+
+    // The html tag used to render my component
+
+    tag: 'my-login-component',
+
+    // The url that will be loaded in the iframe or popup, when someone includes my component on their page
+
+    url: xcomponent.getCurrentScriptDir() + '/login.htm',
+
+    props: {
+
+        prefilledEmail: {
+            type: 'string',
+            required: false
+        },
+
+        onLogin: {
+            type: 'function',
+            required: true
+        }
+    }
+});
