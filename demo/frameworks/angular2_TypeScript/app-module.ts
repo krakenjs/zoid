0d35d80f4e11ed507bd07444c7f1bb1271d82363
Ahmed Elharouny <ahmedelharouny@yahoo.com>
2017-06-30 15:13:47 +1000

    remove automatic driver registeration and simplify getting access to angular 2 component

--- demo/frameworks/angular2_TypeScript/app-module.ts
+++ demo/frameworks/angular2_TypeScript/app-module.ts
@@ -1,17 +1,13 @@
-import {Component, NgModule, ElementRef, NgZone} from '@angular/core';
+import * as ngCore from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import { AppComponent } from './app-component';
 
 declare const xcomponent:any;
 declare const MyLoginXComponent:any;
 
-const MyLoginXComponentModule = MyLoginXComponent
-  .driver('angular2', { Component, NgModule, ElementRef, NgZone, BrowserModule})
-  .driverResults
-  .angular2
-  .module;
+const MyLoginXComponentModule = MyLoginXComponent.driver('angular2', ngCore);
 
-@NgModule({
+@ngCore.NgModule({
   imports: [ BrowserModule, MyLoginXComponentModule ],
   declarations: [ AppComponent ],
   bootstrap: [ AppComponent ]
--- src/drivers/angular2.js
+++ src/drivers/angular2.js
@@ -5,18 +5,10 @@ import { replaceObject } from '../lib';
 export let angular2 = {
 
     global() {
-        return (window.ng && window.ng.core && window.ng.platformBrowser) ? 
-        {
-            Component: window.ng.core.Component,
-            NgModule: window.ng.core.NgModule,
-            ElementRef: window.ng.core.ElementRef,
-            NgZone: window.ng.core.NgZone,
-            BrowserModule: window.ng.platformBrowser.BrowserModule
-        } 
-        : false;
+        return false;
     },
 
-    register(xcomponent, { Component, NgModule, ElementRef, NgZone, BrowserModule }) {
+    register(xcomponent, { Component, NgModule, ElementRef, NgZone }) {
 
         xcomponent.log('initializing angular2 component');
         
@@ -62,7 +54,6 @@ export let angular2 = {
 
 
         const Angular2Module = NgModule({
-            imports: [BrowserModule],
             declarations: [Angular2Component],
             exports: [Angular2Component]
         })
@@ -71,11 +62,7 @@ export let angular2 = {
                 
             }
         });
-        
-        xcomponent.driverResults.angular2 = {
-            module: Angular2Module
-        };
 
-        return xcomponent;
+        return Angular2Module;
     }
 };
