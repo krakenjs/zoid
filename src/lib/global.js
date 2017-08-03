fe5b99e88285f52d4841a12990abdec6c1515e58
Daniel Brain <dbrain@paypal.com>
2017-08-02 19:32:21 -0700

    Add domain guard to globalFor

--- src/lib/global.js
+++ src/lib/global.js
@@ -1,7 +1,12 @@
 
+import { isSameDomain } from 'cross-domain-utils/src';
 import { __XCOMPONENT__ } from '../constants';
 
 export function globalFor(win) {
+
+    if (!isSameDomain(win)) {
+        return;
+    }
 
     if (!win[__XCOMPONENT__]) {
         win[__XCOMPONENT__] = {};
