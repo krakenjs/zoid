6780124b862346f9b438b90a3a2f635e302f1af5
Robert Säll <pr_125@hotmail.com>
2018-04-19 07:50:45 +0000

    Make it optional to include IE bridge support when importing as module (#153)
    * Make it optional to include IE bridge support when importing as module
    
    * Change name of variable deciding if one wants frame support

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 
-module.exports = require('./dist/xcomponent');
-module.exports.default = module.exports;
+if (process.env.XCOMPONENT_FRAME_ONLY) {
+  module.exports = require('./dist/xcomponent.frame');
+  module.exports.default = module.exports;
+} else {
+  module.exports = require('./dist/xcomponent');
+  module.exports.default = module.exports;
+}
