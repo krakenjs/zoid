8786fdda4fe61de029c1dc1e475bd7674a919d51
Daniel Brain <daniel@brainblocks.io>
2019-02-04 19:56:04 -0800

    Export EVENT

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@
 export { PopupOpenError } from 'belter/src';
 export { create, destroyAll, Component } from './component';
 export type { ZoidComponent, ZoidComponentInstance } from './component';
-export { PROP_TYPE, PROP_SERIALIZATION, CONTEXT } from './constants';
+export { PROP_TYPE, PROP_SERIALIZATION, CONTEXT, EVENT } from './constants';
