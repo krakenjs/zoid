a06ea313e4eccc3f11070be892be86ca7e5379f4
Daniel Brain <daniel@brainblocks.io>
2019-02-04 19:47:39 -0800

    Export ZoidComponentInstance type

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@
 
 export { PopupOpenError } from 'belter/src';
 export { create, destroyAll, Component } from './component';
-export type { ZoidComponent } from './component';
+export type { ZoidComponent, ZoidComponentInstance } from './component';
 export { PROP_TYPE, PROP_SERIALIZATION, CONTEXT } from './constants';
