017ea48a6b15656bedbb4ab7617381c0e0b69652
Daniel Brain <daniel@brainblocks.io>
2019-02-05 21:50:10 -0800

    Export RenderOptionsType

--- src/index.js
+++ src/index.js
@@ -3,4 +3,5 @@
 export { PopupOpenError } from 'belter/src';
 export { create, destroyAll, Component } from './component';
 export type { ZoidComponent, ZoidComponentInstance } from './component';
+export type { RenderOptionsType } from './parent';
 export { PROP_TYPE, PROP_SERIALIZATION, CONTEXT, EVENT } from './constants';
