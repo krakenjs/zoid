7a57bb1a433be16b7cd061616999c3d64d07bce4
Daniel Brain <daniel@brainblocks.io>
2019-02-07 21:36:47 -0800

    Format version inline in globals

--- globals.js
+++ globals.js
@@ -2,17 +2,20 @@
 /* eslint import/no-commonjs: off */
 
 const postRobotGlobals = require('post-robot/globals');
-const { getCurrentVersion } = require('grumbler-scripts/config/webpack.config');
 
 const pkg = require('./package.json');
+
+const formatVersion = (version) => {
+    return version.replace(/[^\d]+/g, '_');
+};
 
 module.exports = {
     __POST_ROBOT__: {
         ...postRobotGlobals.__POST_ROBOT__
     },
     __ZOID__: {
-        __VERSION__:           getCurrentVersion(pkg),
-        __GLOBAL_KEY__:        `__zoid_${ getCurrentVersion(pkg) }__`,
+        __VERSION__:           formatVersion(pkg.version),
+        __GLOBAL_KEY__:        `__zoid_${ formatVersion(pkg.version) }__`,
         __POPUP_SUPPORT__:     true,
         __FRAMEWORK_SUPPORT__: false,
         __DEFAULT_CONTAINER__: true,
