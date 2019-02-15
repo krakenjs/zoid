1b5acde576952e457979c7b9353ff671965359f1
Daniel Brain <dbrain@paypal.com>
2019-02-14 18:03:04 -0800

    Do not throw on destroyAll

--- src/component/component.js
+++ src/component/component.js
@@ -417,7 +417,7 @@ export function destroyAll() : ZalgoPromise<void> {
     const global = getGlobal();
     global.activeComponents = global.activeComponents || [];
     while (global.activeComponents.length) {
-        results.push(global.activeComponents[0].destroy());
+        results.push(global.activeComponents[0].destroy(new Error(`zoid desroyed all`), false));
     }
 
     return ZalgoPromise.all(results).then(noop);
