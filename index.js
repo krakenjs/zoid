f00549a073b8604ac2b997442e659cae483776e8
khoa <khoa@Anthonys-MacBook-Pro.local>
2020-10-08 12:00:25 -0700

    closes #291 - use single quotes

--- index.js
+++ index.js
@@ -4,12 +4,12 @@
 // eslint-disable-next-line no-process-env
 if (process && process.env && process.env.ZOID_FRAME_ONLY) {
     // $FlowFixMe
-    module.exports = require("./dist/zoid.frame");
+    module.exports = require('./dist/zoid.frame');
     // $FlowFixMe
     module.exports.default = module.exports;
 } else {
     // $FlowFixMe
-    module.exports = require("./dist/zoid");
+    module.exports = require('./dist/zoid');
     // $FlowFixMe
     module.exports.default = module.exports;
 }
