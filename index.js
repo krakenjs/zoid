8b3208f5c38ec4361e09cafc74c19406b44675d9
anthonykhoa <45890848+anthonykhoa@users.noreply.github.com>
2020-10-09 09:21:33 -0700

    closes #291 - make sure process exists before checking process.env an… (#325)
    * closes #291 - make sure process exists before checking process.env and process.env.XXXX
    
    * closes #291 - use single quotes
    
    * closes #291 - update commit to have travis rebuild
    
    * closes #291 - update commit to have travis rebuild
    
    Co-authored-by: khoa <khoa@Anthonys-MacBook-Pro.local>

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 /* eslint import/no-commonjs: 0 */
 
 // eslint-disable-next-line no-process-env
-if (process.env.ZOID_FRAME_ONLY) {
+if (process && process.env && process.env.ZOID_FRAME_ONLY) {
     // $FlowFixMe
     module.exports = require('./dist/zoid.frame');
     // $FlowFixMe
