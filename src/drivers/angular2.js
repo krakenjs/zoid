031898613968dca14fe55261a6a73a31231d1089
Andre Gardi <agardi@paypal.com>
2021-08-16 16:39:12 -0300

    Update angular2 driver to be compatible with Angular 5 to 12

--- src/drivers/angular2.js
+++ src/drivers/angular2.js
@@ -10,17 +10,18 @@ type Angular2Injection = {||};
 
 type Angular2Component = {||};
 
-type Angular2Module = {||};
+type Angular2Module = {| annotations : Object, name : string |};
 
 type Angular2 = {|
     Component : ({| selector : string, template : string, inputs : $ReadOnlyArray<string> |}) => {|
         Class : ({| constructor : $ReadOnlyArray<Angular2Injection | Function>, ngOnInit : () => void, ngDoCheck : () => void |}) => Angular2Component
     |},
-    NgModule : ({| declarations : $ReadOnlyArray<Angular2Component>, exports : $ReadOnlyArray<Angular2Component> |}) => {|
+    NgModule : ({| declarations : $ReadOnlyArray<*>, exports : $ReadOnlyArray<*> |}) => {|
         Class : ({| constructor : () => void |}) => Angular2Module
     |},
     ElementRef : Angular2Injection,
-    NgZone : Angular2Injection
+    NgZone : Angular2Injection,
+    Inject : Function
 |};
 
 const equals = (obj1, obj2) => {
@@ -47,62 +48,74 @@ const equals = (obj1, obj2) => {
 
 export const angular2 : ComponentDriverType<*, Angular2, Angular2Module, *> = {
 
-    register: (tag, propsDef, init, { Component : AngularComponent, NgModule, ElementRef, NgZone }) => {
-        const getProps = (component) => {
-            return replaceObject({ ...component.internalProps, ...component.props }, item => {
-                if (typeof item === 'function') {
-                    return function angular2Wrapped() : void {
-                        // $FlowFixMe
-                        return component.zone.run(() => item.apply(this, arguments));
-                    };
-                }
-                return item;
-            });
-        };
+    register: (tag, propsDef, init, { Component : AngularComponent, NgModule, ElementRef, NgZone, Inject }) => {
+        class ComponentInstance {
+            elementRef : Object;
+            internalProps : Object;
+            parent : Object;
+            props : Object;
+            zone : Object;
+            _props : Object;
 
-        const ComponentInstance =
-            AngularComponent({
-                selector: tag,
-                template: '<div></div>',
-                inputs:   [ 'props' ]
-            }).Class({
-                constructor: [ ElementRef, NgZone, function angularConstructor(elementRef, zone) {
-                    // $FlowFixMe[object-this-reference]
-                    this._props = {};
-                    // $FlowFixMe[object-this-reference]
-                    this.elementRef = elementRef;
-                    // $FlowFixMe[object-this-reference]
-                    this.zone = zone;
-                } ],
-                ngOnInit () {
-                    // $FlowFixMe[object-this-reference]
-                    const targetElement = this.elementRef.nativeElement;
-                    
-                    // $FlowFixMe[object-this-reference]
-                    this.parent = init(getProps(this));
-                    // $FlowFixMe[object-this-reference]
-                    this.parent.render(targetElement, CONTEXT.IFRAME);
-                },
-                ngDoCheck() {
-                    // $FlowFixMe[object-this-reference]
-                    if (this.parent && !equals(this._props, this.props)) {
-                        // $FlowFixMe[object-this-reference]
-                        this._props = { ...this.props };
-                        // $FlowFixMe[object-this-reference]
-                        this.parent.updateProps(getProps(this));
-                    }
-                }
-            });
+            static annotations : $ReadOnlyArray<*>;
+            static parameters : $ReadOnlyArray<*>;
 
-
-        const ModuleInstance = NgModule({
-            declarations: [ ComponentInstance ],
-            exports:      [ ComponentInstance ]
-        }).Class({
-            constructor () {
-                // pass
+            constructor (elementRef, zone) {
+                this._props = {};
+                this.elementRef = elementRef;
+                this.zone = zone;
             }
-        });
+
+            getProps () : Object {
+                return replaceObject({ ...this.internalProps, ...this.props }, item => {
+                    if (typeof item === 'function') {
+                        const { zone } = this;
+                        return function angular2Wrapped() : void {
+                            // $FlowFixMe
+                            return zone.run(() => item.apply(this, arguments));
+                        };
+                    }
+                    return item;
+                });
+            }
+
+            ngOnInit() {
+                const targetElement = this.elementRef.nativeElement;
+                this.parent = init(this.getProps());
+                this.parent.render(targetElement, CONTEXT.IFRAME);
+            }
+
+            ngDoCheck() {
+                if (this.parent && !equals(this._props, this.props)) {
+                    this._props = { ...this.props };
+                    this.parent.updateProps(this.getProps());
+                }
+            }
+        }
+
+        ComponentInstance.parameters = [
+            [ new Inject(ElementRef) ],
+            [ new Inject(NgZone) ]
+        ];
+
+        ComponentInstance.annotations = [
+            new AngularComponent({
+                selector: tag,
+                template: `<div></div>`,
+                inputs:   [ 'props' ]
+            })
+        ];
+
+        class ModuleInstance {
+            static annotations : $ReadOnlyArray<*>;
+        }
+
+        ModuleInstance.annotations = [
+            new NgModule({
+                declarations: [ ComponentInstance ],
+                exports:      [ ComponentInstance ]
+            })
+        ];
 
         return ModuleInstance;
     }
