54c3f3c0657b821680f5a2b438b20885f6c7b095
artmanque <i@xn--rtman-ewa.com>
2022-03-07 13:38:16 +0500

    fix: npm package paths angular2_Typescript/config

--- demo/frameworks/angular2_TypeScript/config.js
+++ demo/frameworks/angular2_TypeScript/config.js
@@ -12,12 +12,12 @@ System.config({
     map: {
     
         'app': './',
-        '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
-        '@angular/common': 'npm:@angular/common/bundles/common.umd.js',
-        '@angular/compiler': 'npm:@angular/compiler/bundles/compiler.umd.js',
-        '@angular/platform-browser': 'npm:@angular/platform-browser/bundles/platform-browser.umd.js',
-        '@angular/platform-browser-dynamic': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
-        'rxjs': 'npm:rxjs',
+        '@angular/core': 'npm:@angular/core@2/bundles/core.umd.js',
+        '@angular/common': 'npm:@angular/common@2/bundles/common.umd.js',
+        '@angular/compiler': 'npm:@angular/compiler@2/bundles/compiler.umd.js',
+        '@angular/platform-browser': 'npm:@angular/platform-browser@2/bundles/platform-browser.umd.js',
+        '@angular/platform-browser-dynamic': 'npm:@angular/platform-browser-dynamic@2/bundles/platform-browser-dynamic.umd.js',
+        'rxjs': 'npm:rxjs@5.0.0',
         'typescript': 'npm:typescript@2.2.1/lib/typescript.js'
     },
   // packages defines our app package
