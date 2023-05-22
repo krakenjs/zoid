c0f6c2d9c3b8d43add6c5e0c14f033b006c19b48
dtjones404 <dustintaylorjones@gmail.com>
2023-05-22 14:38:55 -0400

    chore: more ci fixes

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 // eslint-disable-next-line no-process-env
 if (process.env.ZOID_FRAME_ONLY) {
     // $FlowFixMe
-    module.exports = require('./dist/zoid.frame');
+    module.exports = require('./dist/zoid.frame'); // eslint-disable-line import/extensions,eslint-comments/no-unused-disable
     module.exports.default = module.exports;
 } else {
     // $FlowFixMe
